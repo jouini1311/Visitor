a3a3c55aa02b1754ce2c2db120b3542d82a85c96
Emma Paris <eparis@atlassian.com>
2024-05-10 22:38:50 +0200

    First Commit

---
+++ src/main/java/refactoring_guru/visitor/example/shapes/Rectangle.java
@@ -0,0 +1,21 @@
+package refactoring_guru.visitor.example.shapes;
+
+import refactoring_guru.visitor.example.visitor.Visitor;
+
+public record Rectangle(int id, int x, int y, int width, int height) implements Shape {
+
+    @Override
+    public String accept(Visitor visitor) {
+        return visitor.visitRectangle(this);
+    }
+
+    @Override
+    public void move(int x, int y) {
+        // move shape
+    }
+
+    @Override
+    public void draw() {
+        // draw shape
+    }
+}
